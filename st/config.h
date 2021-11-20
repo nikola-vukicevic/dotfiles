2dee07f255c892a77b7cf3279bd6ed13615f0505
Nikola Vukićević <39658013+nikola-vukicevic@users.noreply.github.com>
2021-11-20 15:36:04 +0100

    Update config.h

--- st/config.h
+++ st/config.h
@@ -88,6 +88,7 @@ unsigned int alpha = 0xe0;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
+	
 	/* 8 normal colors */
 
 	"#333333",  /*  0: black    */
