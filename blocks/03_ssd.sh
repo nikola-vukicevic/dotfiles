5733577b1ec4d38486b561a49ec1679b31f26bd4
Nikola Vukićević <39658013+nikola-vukicevic@users.noreply.github.com>
2022-08-15 22:44:55 +0200

    Delete 03_ssd.sh

--- blocks/03_ssd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-case $BLOCK_BUTTON in
-	1) st -e lfrun ;;
-esac
-
-ssd_1=$(df -h /dev/nvme0n1p2 | awk '/dev/ {print $4}')
-ssd_2=$(df -h /dev/nvme0n1p3 | awk '/dev/ {print $4}')
-
-echo " 💾 $ssd_1/$ssd_2 "
