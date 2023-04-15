83d93d4d6ff169e0b5b953a3cfecd25e70033302
Nikola Vukićević <39658013+nikola-vukicevic@users.noreply.github.com>
2023-04-15 17:33:36 +0200

    Update 03_plug.lua

--- neovim_init_lua/lua/03_plug.lua
+++ neovim_init_lua/lua/03_plug.lua
@@ -58,7 +58,7 @@ require("lazy").setup({
 		'nvim-telescope/telescope.nvim', tag = '0.1.1',
 		-- or                              , branch = '0.1.1',
 		dependencies = { { 'nvim-lua/plenary.nvim' } }
-    },
+    	},
 	-- ---------------------------------------------
 	-- Vim visual multi (multi cursor):
 	-- ---------------------------------------------
