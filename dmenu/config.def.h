98bf64a963df6e8fcf9f176621318a46b23ad9d5
Nikola Vukićević <39658013+nikola-vukicevic@users.noreply.github.com>
2023-04-24 17:49:31 +0200

    Add files via upload

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 static const unsigned int alpha = 0xf0;
@@ -14,6 +15,8 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 
@@ -25,7 +28,6 @@ static const unsigned int alphas[SchemeLast][2] = {
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 0;
 static unsigned int min_lineheight = 8;
--- dmenu/config.h
+++ dmenu/config.h
@@ -1,31 +1,35 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar               = 1;    /* -b  option; if 0, dmenu appears at bottom     */
-static int centered             = 1;    /* -c option; centers dmenu on screen */
-static int min_width            = 500;  /* minimum width when centered */
+static int topbar               = 1;     /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy                = 1;     /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int centered             = 1;     /* -c option; centers dmenu on screen */
+static int min_width            = 500;   /* minimum width when centered */
 static const unsigned int alpha = 0xf0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=16"
+	"monospace:size=15"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel]  = { "#eeeeee", "#1166c8" }, //#005577
-	[SchemeOut]  = { "#000000", "#00ffff" },
+	[SchemeNorm]          = { "#bbbbbb", "#222222" },
+	[SchemeSel]           = { "#eeeeee", "#1166c8" }, // #005577
+	[SchemeSelHighlight]  = { "#ffc978", "#6b7084" }, // #005577" },
+	[SchemeNormHighlight] = { "#80a0f0", "#2e2e31" }, // #222222" },
+	[SchemeOut]           = { "#000000", "#00ffff" },
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
 	[SchemeNorm] = { OPAQUE, alpha },
-	[SchemeSel] =  { OPAQUE, alpha },
-	[SchemeOut] =  { OPAQUE, alpha },
+	[SchemeSel]  = { OPAQUE, alpha },
+	// [SchemeSelHighlight]  = { OPAQUE, alpha },
+	// [SchemeNormHighlight] = { OPAQUE, alpha },
+	[SchemeOut]  = { OPAQUE, alpha },
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines          = 12;
-
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight     = 32;
 static unsigned int min_lineheight = 8;
