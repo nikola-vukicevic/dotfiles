84b48fdb658e874732b3e2d2066d86e36ac48499
Nikola Vukićević <39658013+nikola-vukicevic@users.noreply.github.com>
2023-05-02 16:27:52 +0200

    Update config.h

--- dmenu/config.h
+++ dmenu/config.h
@@ -8,7 +8,7 @@ static int min_width            = 500;   /* minimum width when centered */
 static const unsigned int alpha = 0xf0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Inconsolata:style=Bold:size=16"
+	"monospace:size=16"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
