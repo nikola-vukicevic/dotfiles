be009449427ccc7a33f9185a9edf760c40d6d14f
Nikola Vukićević <39658013+nikola-vukicevic@users.noreply.github.com>
2025-05-10 20:56:08 +0200

    Update config

--- neovim_init_lua/lua/plugins/dap.lua
+++ neovim_init_lua/lua/plugins/dap.lua
@@ -123,7 +123,7 @@ dap.configurations.python = {
 		type = 'python',
 		request = 'launch',
 		name = "Launch file",
-		program = "${file}";
+		program = "${file}",
 		pythonPath = function()
 			local cwd = vim.fn.getcwd()
 			if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
@@ -133,7 +133,7 @@ dap.configurations.python = {
 			else
 				return '/usr/bin/python'
 			end
-		end;
+		end
 	},
 }
 -- -----------------------------------------------------------------------------
