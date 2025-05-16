1426174794657f1c77c9d7a2aab271ac02e722c4
Nikola Vukićević <39658013+nikola-vukicevic@users.noreply.github.com>
2025-05-16 19:26:40 +0200

    Update config

--- neovim_init_lua/lua/plugins/init.lua
+++ neovim_init_lua/lua/plugins/init.lua
@@ -224,6 +224,7 @@ require("ibl").setup({
 					"interface_declaration",
 					"import_statement",
 					"initializer_list",
+					-- "access_specifier"
 				}
 			},
 		}
@@ -233,9 +234,9 @@ require("ibl").setup({
 require('lint').linters_by_ft = {
 	typescript = { 'eslint_d' },
 	javascript = { 'eslint_d' },
-	python     = { 'pylint' }
+	python     = { 'ruff' }
 }
--- -----------------------------------------------------------------------------
+-- --------------------------
 vim.api.nvim_create_autocmd({
 		-- "BufWritePost",
 		"InsertLeave",
@@ -243,12 +244,8 @@ vim.api.nvim_create_autocmd({
 		"BufEnter"
 	}, {
 		callback = function()
-			-- try_lint without arguments runs the linters defined in `linters_by_ft`
-			-- for the current filetype
 			require("lint").try_lint()
-			-- You can call `try_lint` with a linter name or a list of names to always
-			-- run specific linters, independent of the `linters_by_ft` configuration
-			-- require("lint").try_lint("eslint")
+			-- require("lint").try_lint("eslint_d")
 		end,
 })
 -- -----------------------------------------------------------------------------
