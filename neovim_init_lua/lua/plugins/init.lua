142a355a0f9177ef33cfd6d117bc4638fcb7ad41
Nikola Vukićević <39658013+nikola-vukicevic@users.noreply.github.com>
2026-03-19 21:58:19 +0100

    Add files via upload

--- neovim_init_lua/lua/plugins/init.lua
+++ neovim_init_lua/lua/plugins/init.lua
@@ -81,7 +81,7 @@ vim.diagnostic.config({
 		},
 	},
 	severity_sort = true,
-	-- underline = true,
+	-- underline     = false,
 	-- virtual_text = {
 		-- severity = {
 		-- 	min = vim.diagnostic.severity.WARN,
